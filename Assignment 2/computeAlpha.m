6f0c8b7ea42336459e64495c06c1ac27ed10a5d4
Suvir Jain <suvirj@gmail.com>
2014-02-06 22:37:12 -0800

    Added code to calculate alpha matrices

--- Assignment 2/computeAlpha.m
+++ Assignment 2/computeAlpha.m
@@ -1,13 +1,17 @@
-function x=alpha(M,N)
+function x=computeAlpha(M,N)
 %M : Size of tag set
 %N : Number of words in a sentence
 %Dummy comment
-x=ones(M,N)
+x=ones(M,N);
+x(1,:)=0; % The entire first row of alpha matrix has zeros
+x(1,1)=1; % alpha(0,y)=I(y=START)
+
 for i=1:M % COLUMNS
-    for j=1:N %ROWS
-        if j==0 && i==0
-            x(i,j)=1
-        elseif i==0
+    for j=2:N %ROWS
+        tempsum=0;
+        for k=1:M %sum over all u
+            tempsum = tempsum + x(j-1,k)*exp(getG(i,x(j,k),x(j,i))); %Assume that g is loaded in memory
         end
+        x(j,i)=tempsum;
     end
 end
---
+++ Assignment 2/getG.m
@@ -0,0 +1,3 @@
+function x=getG(i,tag1,tag2)
+%This is a function that returns a 
+    x=g(i,tag1,tag2)
