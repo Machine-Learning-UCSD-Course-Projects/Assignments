fa5b402d457430c46f7a177c1c5b5c249eb8dd6f
nonlogic <kashyap.tumkur@gmail.org>
2014-02-08 19:46:20 -0800

    Fixed A

---
+++ Assignment 2/A.m
@@ -0,0 +1,13 @@
+function [ result ] = A(j, xbar, i)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+    global NUM_FEATURE_TAGS
+    feature = ceil(j / NUM_FEATURE_TAGS);
+    current_feature = xbar(i); % Need some way of loading tagdata for xbar(i)
+    if feature == current_feature
+        result = 1;
+    else
+        result = 0;
+    end;
+end
+
