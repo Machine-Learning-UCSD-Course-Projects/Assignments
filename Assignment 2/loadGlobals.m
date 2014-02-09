cd46e109692333d23972c72206dd106a23a7ed48
Suvir Jain <suvirj@gmail.com>
2014-02-09 14:39:33 -0800

    Changes to global variables

--- Assignment 2/loadGlobals.m
+++ Assignment 2/loadGlobals.m
@@ -1,12 +1,23 @@
 function loadGlobals()
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
-    global NUM_FEATURE_TAGS NUM_LABEL_TAGS FEATURE_TAGS LABEL_TAGS
+    global NUM_FEATURE_TAGS NUM_LABEL_TAGS FEATURE_TAGS LABEL_TAGS ...
+        POS_TRAINING_SENTENCES POS_TRAINING_LABELS ...
+        POS_VALIDATION_SENTENCES POS_VALIDATION_LABELS ...
+        POS_TEST_SENTENCES POS_TEST_LABELS ...
+        AUXILIARY_TRAINING
     
     NUM_FEATURE_TAGS = 36;
     FEATURE_TAGS = 36;
     LABEL_TAGS = { 'START', 'COMMA', 'PERIOD', 'QUESTION_MARK', 'EXCLAMATION_POINT', 'COLON', 'SPACE', 'STOP' };
     NUM_LABEL_TAGS = numel(LABEL_TAGS);
+    POS_TRAINING_SENTENCES = csvread('./CSV/trainingSentences.csv');
+    POS_TRAINING_LABELS = csvread('./CSV/trainingLabels.csv');
+    POS_VALIDATION_SENTENCES = csvread('./CSV/validationSentences.csv');
+    POS_VALIDATION_LABELS = csvread('./CSV/validationLabels.csv');
+    POS_TEST_SENTENCES = csvread('./CSV/testSentences.csv');
+    POS_TEST_LABELS = csvread('./CSV/testLabels.csv');
+    AUXILIARY_TRAINING = csvread('./CSV/auxiliary.csv');
     
 end
 
