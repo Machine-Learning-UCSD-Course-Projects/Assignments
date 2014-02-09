f9096078ec6d5162603355433e54cdc98e7d2f8d
nonlogic <kashyap.tumkur@gmail.org>
2014-02-08 23:51:44 -0800

    Further edits to B.

--- Assignment 2/B.m
+++ Assignment 2/B.m
@@ -3,9 +3,16 @@ function [ result ] = B(j, yiminus1, yi)
 %   Detailed explanation goes here
     global NUM_FEATURE_TAGS NUM_LABEL_TAGS
     
-    section = mod(j, (NUM_LABEL_TAGS ^ 2)) + 1;
+    section = mod(j, (NUM_LABEL_TAGS ^ 2));
+	if section == 0
+		section = (NUM_LABEL_TAGS ^ 2);
+	end;
+	
     prev_tag = ceil(section / NUM_LABEL_TAGS);
-    current_tag = mod(section, NUM_LABEL_TAGS) + 1;
+    current_tag = mod(section, NUM_LABEL_TAGS);
+	if current_tag == 0
+		current_tag = 8;
+	end;
 
     if yiminus1 == prev_tag && yi == current_tag
         result = 1;
