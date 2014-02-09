ee2f8a302ed90d44786ff2b677df60873f7cc94f
nonlogic <kashyap.tumkur@gmail.org>
2014-02-08 19:31:27 -0800

    A() now uses xbar directly.

--- Assignment 2/A.m
+++ Assignment 2/A.m
@@ -3,7 +3,7 @@ function [ result ] = A(j, xbar, i)
 %   Detailed explanation goes here
     global NUM_FEATURE_TAGS
     tag = ceil(j / NUM_FEATURE_TAGS);
-    current_tag = 0; % Need some way of loading tagdata for xbar(i)
+    current_tag = xbar(i); % Need some way of loading tagdata for xbar(i)
     if feature == current_feature
         result = 1;
     else
