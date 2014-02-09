7199717330a1264857acdb36b76dfc1eee8c731a
nonlogic <kashyap.tumkur@gmail.org>
2014-02-08 19:56:29 -0800

    A

--- Assignment 2/A.m
+++ Assignment 2/A.m
@@ -3,7 +3,7 @@ function [ result ] = A(j, xbar, i)
 %   Detailed explanation goes here
     global NUM_FEATURE_TAGS
     feature = ceil(j / NUM_FEATURE_TAGS);
-    current_feature = xbar(i); % Need some way of loading tagdata for xbar(i)
+    current_feature = xbar(i); % Need some way of loading tag data for xbar(i)
     if feature == current_feature
         result = 1;
     else
