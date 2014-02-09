946e161a4222d321eb486d81a775400f21e203bb
Gaurav Saxena <gsaxena81@gmail.com>
2014-02-08 20:22:17 -0800

    SGD and Collins Perceptron

--- Assignment 2/collinsPerceptron.m
+++ Assignment 2/collinsPerceptron.m
@@ -1,26 +1,28 @@
 function sgd(x, trueY)
-    ff = featureFunction();
+   numFF = numFeatureFunctions();
     w = zeroes(numel(ff));
-    iterations = 1000;
+    iterations = 10;
     lambda = 0.1;
+    allY = [1, 2, 3, 4, 5, 6, 7, 8];
     if (size(x,2) + 2) ~= size(trueY, 2)
         error('size(x,2) != size(y, 2)');
     end
+    M = size(y, 2);
+    N = size(x, 2);
     for i = 1:iterations
-        yhat = Inference(x, w);
-        for j = 1:numel(ff)
+        for j = 1:numFF
             yhat = Inference(x, w);
             w(j) = w(j) + lambda * (computeF(ff{j}, x, trueY) - computeF(ff{j}, x, yhat));
         end
     end
 end
 
-function sum = computeF(ffj, x, trueY)
+function sum = computeF(j, x, y)
     sum = 0;
     % Starts from 2 because this contains all the Ys including START and STOP
     % while in notes y_0 and y_{n+1} is assumed outside n tags of Y and
     % words of X
-    for i = 2:size(trueY,2)
-        sum = sum + ffj(trueY(i - 1), trueY(i), x, i);
+    for i = 2:size(y,2)
+        sum = sum + computeFeatureFunction(j, y(i - 1), y(i), x, i);
     end
 end
--- Assignment 2/sgd.m
+++ Assignment 2/sgd.m
@@ -1,24 +1,31 @@
 function sgd(x, trueY)
-    ff = featureFunction();
+    numFF = numFeatureFunctions();
     w = zeroes(numel(ff));
-    iterations = 1000;
+    iterations = 10;
     lambda = 0.1;
+    allY = [1, 2, 3, 4, 5, 6, 7, 8];
     if (size(x,2) + 2) ~= size(trueY, 2)
         error('size(x,2) != size(y, 2)');
     end
+    M = size(y, 2);
+    N = size(x, 2);
     for i = 1:iterations
-        for j = 1:numel(ff)
-            w(j) = w(j) + lambda * (computeF(ff{j}, x, trueY) - computeE());
+        g = computeG(x, allY, w);
+        alpha = computeAlpha(M, N, g);
+        beta = computeBeta(M, N, g);
+        for j = 1:numFF
+            w(j) = w(j) + lambda * (computeF(j, x, trueY)...
+                - computeE(M, N, j, g, x, alpha, beta));
         end
     end
 end
 
-function sum = computeF(ffj, x, trueY)
+function sum = computeF(j, x, trueY)
     sum = 0;
     % Starts from 2 because this contains all the Ys including START and STOP
     % while in notes y_0 and y_{n+1} is assumed outside n tags of Y and
     % words of X
     for i = 2:size(trueY,2)
-        sum = sum + ffj(trueY(i - 1), trueY(i), x, i);
+        sum = sum + computeFeatureFunction(j, trueY(i - 1), trueY(i), x, i);
     end
 end
