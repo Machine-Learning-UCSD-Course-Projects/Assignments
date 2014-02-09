9b6272f25605a5118bd482b3b8e6429f00b218c7
Suvir Jain <suvirj@gmail.com>
2014-02-08 17:07:31 -0800

    fixed bug in computeG.m

--- Assignment 2/computeG.m
+++ Assignment 2/computeG.m
@@ -1,10 +1,10 @@
 function g = computeG(x, allY, w)
     %The equation below equation 4 in the notes Pg 7
-    g = zeros(size(x, 2), size(allY, 2), size(allY, 2));
+    g = zeros(size(allY, 2), size(allY, 2), size(x, 2));
     for i = 1:size(x, 2)
         for u = 1:size(allY, 2)
             for v = 1:size(allY, 2)
-                g(i, u, v) = gi(allY(u), allY(v), x, i, featureFunction(), w);
+                g(u, v, i) = gi(allY(u), allY(v), x, i, featureFunction(), w);
             end
         end
     end
