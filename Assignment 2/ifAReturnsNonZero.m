8f8572251421594a72dc2768721c1737ff43eee4
nonlogic <kashyap.tumkur@gmail.org>
2014-02-09 13:22:31 -0800

    Added ifAReturnsNonZero

---
+++ Assignment 2/ifAReturnsNonZero.m
@@ -0,0 +1,17 @@
+function [ result ] = ifAReturnsNonZero(j, xbar)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+    global NUM_LABEL_TAGS
+    current_feature = ceil(j / (NUM_LABEL_TAGS ^ 2));
+    result = 0;
+    if current_feature > 36
+        result = 1;
+        return;
+    end;
+    for i = 1:size(xbar, 2)
+        if current_feature == xbar(i)
+            result = 1;
+        end;
+    end;
+end
+
