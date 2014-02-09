a5716ecb2809cfd83d0162b644e5bca272d45159
Gaurav Saxena <gsaxena81@gmail.com>
2014-02-08 22:51:42 -0800

    SGD

--- Assignment 2/sgd.m
+++ Assignment 2/sgd.m
@@ -1,21 +1,24 @@
-function sgd(x, trueY)
+function w = sgd(sentences, trueY)
+    if (size(sentences, 2) + 2) ~= size(trueY, 2)
+        error('size(x,2) + 2 != size(y, 2)');
+    end
     numFF = numFeatureFunctions();
-    w = zeroes(numel(ff));
-    iterations = 10;
+    w = zeros(numFF);
+    epochs = 1;
     lambda = 0.1;
     allY = [1, 2, 3, 4, 5, 6, 7, 8];
-    if (size(x,2) + 2) ~= size(trueY, 2)
-        error('size(x,2) != size(y, 2)');
-    end
-    M = size(y, 2);
-    N = size(x, 2);
-    for i = 1:iterations
-        g = computeG(x, allY, w);
-        alpha = computeAlpha(M, N, g);
-        beta = computeBeta(M, N, g);
-        for j = 1:numFF
-            w(j) = w(j) + lambda * (computeF(j, x, trueY)...
-                - computeE(M, N, j, g, x, alpha, beta));
+    for i = 1:epochs
+        for l = 1:size(sentences, 2)
+            x = sentences(l);
+            M = size(allY, 2);
+            N = size(x, 2);
+            g = computeG(x, allY, w);
+            alpha = computeAlpha(M, N, g);
+            beta = computeBeta(M, N, g);
+            for j = 1:numFF
+                w(j) = w(j) + lambda * (computeF(j, x, trueY)...
+                    - computeE(M, N, j, g, x, alpha, beta));
+            end
         end
     end
 end
@@ -25,7 +28,7 @@ function sum = computeF(j, x, trueY)
     % Starts from 2 because this contains all the Ys including START and STOP
     % while in notes y_0 and y_{n+1} is assumed outside n tags of Y and
     % words of X
-    for i = 2:size(trueY,2)
-        sum = sum + computeFeatureFunction(j, trueY(i - 1), trueY(i), x, i);
+    for i = 1:size(x,2)
+        sum = sum + computeFeatureFunction(j, trueY(i), trueY(i + 1), x, i);
     end
 end
