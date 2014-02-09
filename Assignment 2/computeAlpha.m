74d2f3f42007495acd7e8a8cabd132170b313727
Suvir Jain <suvirj@gmail.com>
2014-02-08 17:53:13 -0800

    Fixed notation in computing alpha

--- Assignment 2/computeAlpha.m
+++ Assignment 2/computeAlpha.m
@@ -7,11 +7,11 @@ alpha(1,:)=0; % The entire first row of alpha matrix has zeros
 alpha(1,1)=1; % alpha(0,y)=I(y=START)
 
 for v=1:M % COLUMNS
-    for k=2:N %ROWS
+    for k=1:N-1 %ROWS
         tempsum=0;
         for u=1:M %sum over all u
-            tempsum = tempsum + alpha(k-1,u)*exp(g(u,v,k)); %Assume that g is loaded in memory
+            tempsum = tempsum + alpha(k,u)*exp(g(u,v,k+1)); %Assume that g is loaded in memory
         end
-        alpha(k,v)=tempsum;
+        alpha(k+1,v)=tempsum;
     end
 end
