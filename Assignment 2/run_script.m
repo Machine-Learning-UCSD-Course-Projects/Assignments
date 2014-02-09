8a3549b6cf767128dd295a904f533ddd652b6e68
Suvir Jain <suvirj@gmail.com>
2014-02-08 19:45:12 -0800

    Run_script now handles 2 sentences!

--- Assignment 2/run_script.m
+++ Assignment 2/run_script.m
@@ -3,8 +3,9 @@ loadGlobals();
 j = 1296;
 
 %Input Sentence
-x = [1 2 3 4 8];
-xbar = x;
+x = ones(2,5);
+x(1,:)=[1 2 3 4 8];
+x(2,:)=[1 3 3 4 8];
 N = size(x,2);
 
 %All the 8 tags
@@ -14,28 +15,31 @@ M = size(y,2);
 %Weight vector
 w = ones(1,j)*0.1;
 
-%Compute all gi matrices
-g = computeG(x, y, w);
+for i=1:size(x,1)
+    xbar = x(i,:);
+    %Compute all gi matrices
+    g = computeG(xbar, y, w);
 
-%Compute alpha matrix
-alpha = computeAlpha(M,N,g);
+    %Compute alpha matrix
+    alpha = computeAlpha(M,N,g);
 
-%Compute beta matrix
-beta = computeBeta(M,N,g);
+    %Compute beta matrix
+    beta = computeBeta(M,N,g);
 
-%Compute Z using alpha matrix
-z1 = computeZ_usingAlpha(alpha,M,N);
+    %Compute Z using alpha matrix
+    z1 = computeZ_usingAlpha(alpha,M,N);
 
-%Compute Z using beta matrix
-z2 = computeZ_usingBeta(beta,g,M);
+    %Compute Z using beta matrix
+    z2 = computeZ_usingBeta(beta,g,M);
 
-%Sanity Check
-if z1 - z2 > 1
-    error('Z values are different');
+    %Sanity Check
+    if z1 - z2 > 1
+        error('Z values are different');
+    end
+
+    %Initialize the actual Z
+    Z = z1;
+
+    %Calculate E
+    E = computeE(M,N,j,g,xbar,alpha,beta,Z)
 end
-
-%Initialize the actual Z
-Z = z1;
-
-%Calculate E
-E = computeE(M,N,j,g,xbar,alpha,beta,Z);
