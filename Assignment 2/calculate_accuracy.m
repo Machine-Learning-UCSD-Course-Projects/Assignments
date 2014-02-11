7a57608e2d8e5940a19e422f7bc2e07565037698
Suvir Jain <suvirj@gmail.com>
2014-02-11 02:55:50 -0800

    few more files

--- Assignment 2/calculate_accuracy.m
+++ Assignment 2/calculate_accuracy.m
@@ -1,5 +1,8 @@
 %Test the Infered values against the real thing
 %Assume that a weight vector w exists in memory
+SAMPLESIZE = 100;
+TOTAL_LENGTH = 0;
+TAG_LEVEL_MATCH = 0;
 accuracy = 0;
 for i=1:SAMPLESIZE
     x=POS_TRAINING_SENTENCES(i,:);
@@ -8,10 +11,20 @@ for i=1:SAMPLESIZE
     y(y==0)=[];
     y=cat(2,1,y+1,8);
     yhat=Inference(x,w);
-    if yhat == y'
+    
+    
+    if numel(yhat)==numel(y') 
+    TAG_LEVEL_MATCH = TAG_LEVEL_MATCH + nnz(yhat == y');
+    TOTAL_LENGTH = TOTAL_LENGTH + numel(y);
+        if isequal(yhat,y')
         accuracy = accuracy + 1;
     else
         disp(i);
+        end
     end
+    
 end
-disp(accuracy)
+disp('SENTENCE LEVEL ACCURACY');
+disp(accuracy/SAMPLESIZE)
+disp('TAG LEVEL ACCURACY');
+disp(TAG_LEVEL_MATCH/TOTAL_LENGTH);
