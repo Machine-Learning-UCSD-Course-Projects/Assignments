744d8fa08062c82051e7a034dd8efe7200a7969e
Suvir Jain <suvirj@gmail.com>
2014-02-22 21:17:36 -0800

    Changes

--- Assignment 3/doGibbsSampling.m
+++ Assignment 3/doGibbsSampling.m
@@ -1,4 +1,4 @@
-function [theta, n, nsum] = doGibbsSampling(q, n, M, K, V, classic400, z)
+function [theta, phi, n, nsum] = doGibbsSampling(q, n, M, K, V, classic400, z)
     p = zeros(K, 1);
     alpha = ones(K, 1);
     alpha(:) = 0.1;
@@ -6,7 +6,7 @@ function [theta, n, nsum] = doGibbsSampling(q, n, M, K, V, classic400, z)
     beta(:) = 2;
     qsum = zeros(K, 1);
     nsum = zeros(M, 1);
-    ITERATIONS = 20;
+    ITERATIONS = 1;
     
 %     for i = 1:V
 %         for j = 1:K
@@ -30,13 +30,29 @@ function [theta, n, nsum] = doGibbsSampling(q, n, M, K, V, classic400, z)
             end
         end
     end
-
     
     for j = 1:K
-        qsum(j) = sum(q(j, :));
+    %Optimization 3
+    %((qsum(j) + sum(beta))
+        qsum(j) = sum(q(j, :))+sum(beta);
     end
+    
     for m = 1:M
-        nsum(m) = sum(n(m,:));
+    %Optimization 4
+    %(nsum(m) + sum(alpha))
+        nsum(m) = sum(n(m,:))+sum(alpha);
+    end
+    
+    %Optimization 1
+    %(q(j, doc{m}(i)) + beta(i))
+    for row = 1:K
+        q(row,:) = q(row,:)+beta';
+    end
+    
+    %Optimization 2
+    %(n(m, j) + alpha(j))
+    for col = 1:K
+        n(:,col) = n(:,col)+alpha(col);
     end
     for it = 1: ITERATIONS
         it
@@ -47,12 +63,12 @@ function [theta, n, nsum] = doGibbsSampling(q, n, M, K, V, classic400, z)
                 n(m, topic) = n(m, topic) - 1;
                 qsum(topic) = qsum(topic) - 1;
                 nsum(m) = nsum(m) - 1;
+              
                 for j = 1:K
-                        p(j) = (q(j, doc{m}(i)) + beta(i)) * (n(m, j) + alpha(j)) / ...
-                            ((qsum(j) + sum(beta)) * (nsum(m) + sum(alpha)));
-%                         p(i, j) = (q(j, i)) * n(m, j)/ ...
-%                         (qsum(j) * nsum(m));
+                    %INNER LOOP
+                       p(j) = (q(j,doc{m}(i))*n(m, j))/(qsum(j) * nsum(m));
                 end
+                
                 topic = randomTopic(p);
                 q(topic, doc{m}(i)) = q(topic, doc{m}(i)) + 1;
                 n(m, topic) = n(m, topic) + 1;
@@ -71,6 +87,31 @@ function [theta, n, nsum] = doGibbsSampling(q, n, M, K, V, classic400, z)
 %         nsum(m, 1) = sum(n(m,:));
 %     end
     theta = getTheta(M, K, n, nsum);
+    phi = getPhi(doc,M,K,V,z);
+    
+    theta
+    phi
+end
+
+function phi = getPhi(doc,M,K,V,z)
+    phi = ones(K,V);
+    for k = 1:K  
+        k
+        for j=1:V
+            j
+            num = 0;
+            den = 0;
+            for m=1:M
+                for i=1:size(doc{m})
+                num = num + (doc{m}(i) == j && z{m}(i) == k);
+                den = den + (z{m}(i) == k);                
+                end
+            end
+            phi(k,j)=num/den;
+        end
+        %Sort the K rows of phi
+        sort(phi(k,:),'descend');
+    end
 end
 
 function topic = randomTopic(p)
--- Assignment 3/runLDA.m
+++ Assignment 3/runLDA.m
@@ -7,4 +7,9 @@ K = 3;
 if exist('q') == 0
     [ q, n, z ] = initializeLDA(M, K, V, classic400);
 end
-[theta, n, nsum] = doGibbsSampling(q, n, M, K, V, classic400, z);
+[theta, phi, n, nsum] = doGibbsSampling(q, n, M, K, V, classic400, z);
+
+[a,trueLabelsCalc] = max(theta,[],2);
+sum(trueLabelsCalc==1)
+sum(trueLabelsCalc==2)
+sum(trueLabelsCalc==3)
