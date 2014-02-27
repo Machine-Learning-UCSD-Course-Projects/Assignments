50702e047086afc8590ec3f03a763c7c673afe14
Gaurav Saxena <gsaxena81@gmail.com>
2014-02-27 00:31:45 -0800

    Changes

--- Assignment 3/runLDA.m
+++ Assignment 3/runLDA.m
@@ -4,14 +4,18 @@
 % M = size(classic400, 1);
 % end
 clear
-load('hookah3k.mat')
+load('classic400.mat')
 
 % CAREFUL
-classic400=c;
+c = classic400;
 
 V = size(c,2);
 M = size(c,1);
+<<<<<<< Updated upstream
 K = 100;
+=======
+K = 100;%3, 10, 50, 100
+>>>>>>> Stashed changes
 if exist('q') == 0
     [ q, n, z ] = initializeLDA(M, K, V, classic400);
 end
@@ -22,5 +26,9 @@ sum(trueLabelsCalc==1)
 sum(trueLabelsCalc==2)
 sum(trueLabelsCalc==3)
 
+<<<<<<< Updated upstream
 %NOTE : Value at index i in Phi corresponds to topic at index i in dominant_topics
 save('k=100.mat')
+=======
+save('k=100.mat')%3, 10, 50, 100
+>>>>>>> Stashed changes
