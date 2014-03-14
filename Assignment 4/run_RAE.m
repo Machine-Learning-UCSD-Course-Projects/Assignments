e5f858041e9d54080f55ff25a1a364fe9b5e2da5
Suvir Jain <suvirj@gmail.com>
2014-03-13 22:31:56 -0700

    minor changes

--- Assignment 4/run_RAE.m
+++ Assignment 4/run_RAE.m
@@ -3,7 +3,7 @@ clear
 ITER = 4;
 for iterations = 1:ITER
     [ X,W,U,V,d,N,Truelabels,sentence_sizes,positive_sentences,negative_sentences,Vocab,alpha,lambda ] = initialize_all();    
-    N=10;
+    N=100;
     [ T,Roots,Vocab ] = buildAllTrees( X,W,U,sentence_sizes,N,d,positive_sentences,negative_sentences,Vocab );
     disp('All trees built');
     for i = 1 : size(Vocab, 1)
@@ -12,8 +12,8 @@ for iterations = 1:ITER
     %Now running LBFGS
     [W, U, V]  = lbfgsWithoutKidsUpdate(W, U, V, alpha, lambda, Roots, Truelabels, T, Vocab);
 
-    Vocab = lbfgsKidsUpdate(positive_sentences, Truelabels, W, V, Vocab);%Replace positive_sentences by all sentences
-    X = buildX(N,Vocab,positive_sentences,negative_sentences,sentence_sizes );
+    %Vocab = lbfgsKidsUpdate(positive_sentences, Truelabels, W, V, Vocab);%Replace positive_sentences by all sentences
+    %X = buildX(N,Vocab,positive_sentences,negative_sentences,sentence_sizes );
 
 %     Vocab = lbfgsKidsUpdate(Truelabels, W, V, Vocab); %Replace positive_sentences by all sentences
 %     X = buildX(Vocab,positive_sentences,negative_sentences,sentence_sizes );
