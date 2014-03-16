44569d508a4e622c42b30ad233c037f8ef036d6b
Gaurav Saxena <gsaxena81@gmail.com>
2014-03-16 10:18:00 -0700

    bow

--- Assignment 4/bow_LogisticReg.m
+++ Assignment 4/bow_LogisticReg.m
@@ -6,16 +6,22 @@ function [loglikelihood, gradient] = bowLogisticReg(w,allSNum,y, d)
     for i = 1:size(allSNum,1)
         for j = 1:size(allSNum{i}, 2)
             x_start = allSNum{i}(j);
-            x_start = (x_start + 1) * d + 1;
+            x_start = (x_start + 1) * d + 1;%?
             X = w(x_start:(x_start + d - 1));
-            loglikelihood = loglikelihood -1*sum(log(1./(1+exp(-V*X))));
+            loglikelihood = loglikelihood - sum(y(i) * log(1./(1+exp(-V*X)))) - sum((1 - y(i)) * (1 - log(1./(1+exp(-V*X)))));           
             for k = 1:d
-                add = (0 - (1 ./ 1 + exp(-V(1, :) * X))) * V(1, k);
-                gradient(x_start + k - 1, 1) = gradient(x_start + k - 1, 1) + add;
-                add = (1 - (1 ./ 1 + exp(-V(2, :) * X))) * V(2, k);
-                gradient(x_start + k - 1, 1) = gradient(x_start + k - 1, 1) + add;
-                add = (y(i) - (1 ./ 1 + exp(-V(y(i) + 1, :) * X))) * X(k);
-                gradient((y(i) * d) + k, 1) = gradient((y(i) * d) + k, 1) + add;
+                gradient(x_start + k - 1, 1) = gradient(x_start + k - 1, 1) + (y(i) - (1 ./ 1 + exp(-V(1, :) * X))) * V(1, k) + (y(i) - (1 ./ 1 + exp(-V(1, :) * X))) * V(2, k);
+                gradient(k, 1) = gradient(k, 1) + (y(i) - (1 ./ 1 + exp(-V(2, :) * X))) * X(k);
+                gradient(d + k, 1) = gradient(d + k, 1) + (y(i) - (1 ./ 1 + exp(-V(2, :) * X))) * X(k);
+                
+%                 add = (y(i) - (1 ./ 1 + exp(-V(1, :) * X))) * V(1, k);
+%                 gradient(x_start + k - 1, 1) = gradient(x_start + k - 1, 1) + add;
+%                 add = ((1 - y(i)) - (1 ./ 1 + exp(-V(2, :) * X))) * V(1, k);
+%                 gradient(x_start + k - 1, 1) = gradient(x_start + k - 1, 1) + add;
+%                 add = ((1 - y(i)) - (1 ./ 1 + exp(-V(1, :) * X))) * X(k);
+%                 gradient(x_start + k - 1, 1) = gradient(x_start + k - 1, 1) + add;
+%                 add = (y(i) - (1 ./ 1 + exp(-V(2, :) * X))) * X(k);
+%                 gradient((y(i) * d) + k, 1) = gradient((y(i) * d) + k, 1) + add;
             end;
         end;
     end;
--- Assignment 4/bow_runlbfgs.m
+++ Assignment 4/bow_runlbfgs.m
@@ -1,7 +1,9 @@
     options = [];
-    options.display = 'none';
+    %options.display = 'none';
     options.maxFunEvals = '100';
-
-    w_init = ones(14045 * 2, 1);
-    w_final = minFunc(@bow_LogisticReg, w_init, options, allSNum, labels, 2);
+    %options.DerivativeCheck = 'on';
+    d = 2;
+    w = 14045;
+    w_init = rand(w * d, 1) * 2 * 0.05 - 0.05;
+    w_final = minFunc(@bow_LogisticReg, w_init, options, allSNum(1:2), labels, 2);
         
