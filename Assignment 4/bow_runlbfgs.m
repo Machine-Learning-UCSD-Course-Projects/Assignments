bb98921f5cc32569a9c0cd22683368775651fda6
nonlogic <kashyap.tumkur@gmail.org>
2014-03-17 00:32:37 -0700

    BOW

--- Assignment 4/bow_runlbfgs.m
+++ Assignment 4/bow_runlbfgs.m
@@ -2,10 +2,10 @@
     %options.display = 'none';
     options.maxFunEvals = '100';
     %options.DerivativeCheck = 'on';
-    d = 2;
+    d = 10;
     w = 14045;
     w_init = rand(w * d, 1) * 2 * 0.05 - 0.05;
-    %train = allSNum{1:9656};
+    %train = allSNum{1:1024};
     %w_init = ones(w * d, 1);
-    w_final = minFunc(@bow_LogisticReg, w_init, options, allSNum, labels, 2);
+    w_final = minFunc(@bow_LogisticReg, w_init, options, train, trainlabels, 2);
         
--- Assignment 4/bow_runtest.m
+++ Assignment 4/bow_runtest.m
@@ -1,18 +1,18 @@
-d = 2;
+d = 10;
 V(1,:) = w_final(1:d)';
 V(2,:) = w_final((d+1):(2*d))';
-predlabels = [];
-for i = 1:size(allSNum,1)
-    X = zeros(2, 1);
-    for j = 1:size(allSNum{i}, 2)
-        x_start = allSNum{i}(j);
+predlabel = [];
+for i = 1:size(train,1)
+    X = zeros(d, 1);
+    for j = 1:size(train{i}, 2)
+        x_start = train{i}(j);
         x_start = (x_start + 1) * d + 1;
         X = X + w_final(x_start:(x_start + d - 1));
     end;
     X = X / j;
     predlabel(i,:) = 1/(1 + exp(-V * X));
 end;
-
+disp(i)
 [ m, arg ] = max(predlabel');
 arg = arg - 1;
-sum(abs(labels - arg))
+sum(abs(trainlabels - arg))
